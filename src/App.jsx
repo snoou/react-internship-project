ef9e25b76792f24afb757b8fc27e5c4055987f3f
snoou <sajjadyarahmadi100@gmail.com>
2025-10-29 22:08:30 +0330

    update delet

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,13 @@ import './App.css';
 import { useState, useEffect } from 'react';
 import TransactionTable from './components/TransactionTable/TransactionTable';
 import GetInitialTransactions from './utils/GetInitialTransactions/GetInitialTransactions';
-import DeleteTransaction from './utils/DeleteTransaction/DeleteTransaction';
 
 function App() {
+  const deleteTransaction = (id) => {
+    setTransactions((prevTransactions) =>
+      prevTransactions.filter((tx) => tx.id !== id)
+    );
+  };
   const STORAGE_KEY = 'expenseTrackerData';
   const [transactions, setTransactions] = useState(GetInitialTransactions);
   useEffect(() => {
@@ -19,7 +23,7 @@ function App() {
   };
   return (
     <>
-      <TransactionTable data={transactions} onAddTransaction={addTransaction}  onDeleteTransaction={DeleteTransaction} ></TransactionTable>
+      <TransactionTable data={transactions} onAddTransaction={addTransaction} onDeleteTransaction={deleteTransaction} ></TransactionTable>
     </>
   );
 }
--- src/utils/DeleteTransaction/DeleteTransaction.jsx
+++
@@ -1,7 +0,0 @@
-const DeleteTransaction = (id) => {
-    setTransactions((prevTransactions) =>
-        prevTransactions.filter((tx) => tx.id !== id)
-    );
-};
-
-export default DeleteTransaction
