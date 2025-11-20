19ac92f6d130b753b7ae475426d223c92bd524c2
snoou <sajjadyarahmadi100@gmail.com>
2025-11-20 10:37:39 +0330

    creat diagram

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,10 +1,16 @@
 import './Dashboard.css'
+import Diagram from '../Diagram/Diagram';
+import UseContext from '../../utils/UseContext';
 const Dashboard = () => {
+  const [income, expense] = UseContext()
   return (
     <>
       <div className="size">
         <div className="header">
           <h2>داشبورد</h2>
+          <Diagram income={income} expense={expense}></Diagram>
+
+
         </div>
       </div>
     </>
